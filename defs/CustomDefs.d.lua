a73227c08325373eeca37f2531b7f70fe326694f
Can Yilmaz <can@yilmaz.app>
2025-08-30 00:51:59 +0100

    improve IsInInstance defs

--- defs/CustomDefs.d.lua
+++ defs/CustomDefs.d.lua
@@ -5,7 +5,13 @@ GameFontGreenSmall = GameFontGreenSmall
 ---@type Font
 GameFontRedSmall = GameFontRedSmall
 
+---@alias InstanceType
+---| "none" # player is not in an instance
+---| "party" # player in a 5-man instance
+---| "raid" # player in a battleground
+---| "pvp" # player in a raid instance
+
 ---Returns basic information about the instance, if the player is in one.
 ---@return number|nil isInInstance `1` if player is inside an instance, `nil` otherwise
----@return "none"|"party"|"raid"|"pvp" instanceType `"none"` when outside an instance, `"party"` when in a 5-man instance, `"pvp"` when in a battleground, `"raid"` when in a raid instance
+---@return InstanceType instanceType
 function IsInInstance() end
